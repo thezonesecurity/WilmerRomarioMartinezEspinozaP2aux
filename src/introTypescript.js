6cac7f3d3bab7e3ec6c2fb321effd4b13416a495
Romario Martinez <kids6352@gmail.com>
2021-05-18 16:33:23 -0400

    parte1e-1f

--- src/introTypescript.js
+++ src/introTypescript.js
@@ -2,13 +2,31 @@
 var namee = "wilmer romario";
 namee = "15";
 //parte 1b
-function greet(nom, edad) {
-    return "hello " + nom + ", your age is " + edad;
+/*
+function greet(nom:string, edad: number) {
+    return `hello ${nom}, your age is ${edad}`;
+  
 }
-var saludo = greet("Josie", 25);
-//console.log(saludo);
+let saludo = greet("Josie",25);
+//console.log(saludo); */
 //parte 1c
 function validacionNever() {
     while (true) {
     }
 }
+//parte 1d
+/*
+function greet(nom:string, edad: number): never {
+    return `hello ${nom}, your age is ${edad}`;
+  
+}
+let saludo = greet("Josie",25);
+console.log(saludo); */
+// parte 1e-1f
+function mostrarsaludo() {
+    return "Saludos Marie...";
+}
+function greet(f, nom, edad) {
+    return f() + ("\n hello " + nom + ", your age is " + edad);
+}
+console.log(greet(mostrarsaludo, "Josie", 25));
--- src/introTypescript.ts
+++ src/introTypescript.ts
@@ -3,12 +3,13 @@ var namee: string = "wilmer romario";
 namee = "15";
 
 //parte 1b
+/*
 function greet(nom:string, edad: number) {
     return `hello ${nom}, your age is ${edad}`;
   
 }
 let saludo = greet("Josie",25);
-//console.log(saludo);
+//console.log(saludo); */
 
 //parte 1c
 function validacionNever(): never {
@@ -16,3 +17,21 @@ function validacionNever(): never {
 
    }
 }
+
+//parte 1d
+/*
+function greet(nom:string, edad: number): never {
+    return `hello ${nom}, your age is ${edad}`;
+  
+}
+let saludo = greet("Josie",25);
+console.log(saludo); */
+
+// parte 1e-1f
+function mostrarsaludo(): string{
+    return "Saludos Marie..."
+}
+function greet(f: () => string, nom:string, edad: number) {
+    return  f() +`\n hello ${nom}, your age is ${edad}`;
+}
+console.log( greet(mostrarsaludo, "Josie", 25));
