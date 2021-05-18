7f3cb596119c8bef95bd689603cdd3b346db4475
Romario Martinez <kids6352@gmail.com>
2021-05-18 15:39:39 -0400

    parte1c

--- src/introTypescript.js
+++ src/introTypescript.js
@@ -1,9 +1,14 @@
-//parte1a
+//parte 1a
 var namee = "wilmer romario";
 namee = "15";
-//parte1b
+//parte 1b
 function greet(nom, edad) {
     return "hello " + nom + ", your age is " + edad;
 }
 var saludo = greet("Josie", 25);
-console.log(saludo);
+//console.log(saludo);
+//parte 1c
+function validacionNever() {
+    while (true) {
+    }
+}
--- src/introTypescript.ts
+++ src/introTypescript.ts
@@ -1,10 +1,18 @@
-//parte1a
+//parte 1a
 var namee: string = "wilmer romario";
 namee = "15";
-//parte1b
+
+//parte 1b
 function greet(nom:string, edad: number) {
     return `hello ${nom}, your age is ${edad}`;
   
 }
 let saludo = greet("Josie",25);
-console.log(saludo);
+//console.log(saludo);
+
+//parte 1c
+function validacionNever(): never {
+   while(true){
+
+   }
+}
