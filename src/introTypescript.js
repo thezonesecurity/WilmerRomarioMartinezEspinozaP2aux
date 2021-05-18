39ff8f33d4332f50b2ee19c9c99f40cb51a4214c
Romario Martinez <kids6352@gmail.com>
2021-05-18 16:48:28 -0400

    parte1h

--- src/introTypescript.js
+++ src/introTypescript.js
@@ -1,32 +1,42 @@
-//parte 1a
-var namee = "wilmer romario";
-namee = "15";
-//parte 1b
 /*
+//parte 1a
+var namee: string = "wilmer romario";
+namee = "15";
+
+//parte 1b
 function greet(nom:string, edad: number) {
     return `hello ${nom}, your age is ${edad}`;
   
 }
 let saludo = greet("Josie",25);
-//console.log(saludo); */
+console.log(saludo);
+
 //parte 1c
-function validacionNever() {
-    while (true) {
-    }
+function validacionNever(): never {
+   while(true){
+
+   }
 }
+
 //parte 1d
-/*
 function greet(nom:string, edad: number): never {
     return `hello ${nom}, your age is ${edad}`;
-  
 }
 let saludo = greet("Josie",25);
-console.log(saludo); */
+console.log(saludo);
+
 // parte 1e-1f
-function mostrarsaludo() {
-    return "Saludos Marie...";
+function mostrarsaludo(): string{
+    return "Saludos Marie..."
 }
-function greet(f, nom, edad) {
-    return f() + ("\n hello " + nom + ", your age is " + edad);
+function greet(f: () => string, nom:string, edad: number) {
+    return  f() +`\n hello ${nom}, your age is ${edad}`;
 }
-console.log(greet(mostrarsaludo, "Josie", 25));
+console.log( greet(mostrarsaludo, "Josie", 25));
+*/
+// parte1h
+function greet(nom, edad, year) {
+    return "hello " + nom + ", your age is " + edad + " and year is " + year;
+}
+var saludo = greet("Josie", 25, "2021");
+console.log(saludo);
--- src/introTypescript.ts
+++ src/introTypescript.ts
@@ -1,15 +1,15 @@
+/*
 //parte 1a
 var namee: string = "wilmer romario";
 namee = "15";
 
 //parte 1b
-/*
 function greet(nom:string, edad: number) {
     return `hello ${nom}, your age is ${edad}`;
   
 }
 let saludo = greet("Josie",25);
-//console.log(saludo); */
+console.log(saludo);
 
 //parte 1c
 function validacionNever(): never {
@@ -19,13 +19,11 @@ function validacionNever(): never {
 }
 
 //parte 1d
-/*
 function greet(nom:string, edad: number): never {
-    return `hello ${nom}, your age is ${edad}`;
-  
+    return `hello ${nom}, your age is ${edad}`; 
 }
 let saludo = greet("Josie",25);
-console.log(saludo); */
+console.log(saludo); 
 
 // parte 1e-1f
 function mostrarsaludo(): string{
@@ -35,3 +33,11 @@ function greet(f: () => string, nom:string, edad: number) {
     return  f() +`\n hello ${nom}, your age is ${edad}`;
 }
 console.log( greet(mostrarsaludo, "Josie", 25));
+*/
+
+// parte1h
+function greet(nom:string, edad: number, year: number | string) {
+    return `hello ${nom}, your age is ${edad} and year is ${year}`;
+}
+let saludo = greet("Josie", 25, "2021");
+console.log(saludo);
